3f71a09e0317d429b6997f95db3e4ff32572ca85
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-18 23:19:24 +0530

    boxed layout set on app.js

--- findway/src/App.js
+++ findway/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="col-md-9" style={{backgroundColor:"#808080", padding :5}}>
+          {[...Array(25)].map((data, index) => {
+            return <div className="d-flex" key={index}>
+              {[...Array(30)].map((sdata, sindex) => {
+                return <div id={"box" + sindex} key={sindex} style={{ borderRadius: 5,border: "1px solid #000" ,height: 25, width: 10 ,backgroundColor : "transparent" }} className="spacingbox flex-fill">&nbsp;</div>
+              })}
+            </div>
+          })}
+        </div>
+        <div className="col-md-3">&nbsp;</div>
+      </div>
     </div>
   );
 }
