46b3b8d3afb7c00770e3d4e1a38e9e0e51e813cb
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-19 15:51:34 +0530

    set upn the board for game

--- findway/src/App.js
+++ findway/src/App.js
@@ -5,16 +5,18 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="col-md-9" style={{backgroundColor:"#808080", padding :5}}>
-          {[...Array(25)].map((data, index) => {
-            return <div className="d-flex" key={index}>
-              {[...Array(30)].map((sdata, sindex) => {
-                return <div id={"box" + sindex} key={sindex} style={{ borderRadius: 5,border: "1px solid #000" ,height: 25, width: 10 ,backgroundColor : "transparent" }} className="spacingbox flex-fill">&nbsp;</div>
+        <div className="row">
+          <div className="col-md-9" style={{ backgroundColor: "#808080", padding: 5 }}>
+              {[...Array(20)].map((data, index) => {
+                return <div className="d-flex" key={index}>
+                  {[...Array(30)].map((sdata, sindex) => {
+                    return <div id={"box" + sindex} key={sindex} style={{ borderRadius: 5, border: "1px solid #000", height: 25, width: 10, backgroundColor: "transparent" }} className="spacingbox flex-fill">&nbsp;</div>
+                  })}
+                </div>
               })}
             </div>
-          })}
+          <div className="col-md-3">&nbsp;</div>
         </div>
-        <div className="col-md-3">&nbsp;</div>
       </div>
     </div>
   );
