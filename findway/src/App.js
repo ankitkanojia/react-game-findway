9f2f23af6e8e24258dd15a8aca76c78b28f6637a
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-19 20:18:52 +0530

    Insert blocks and cracktus images based on blocks

--- findway/src/App.js
+++ findway/src/App.js
@@ -4,11 +4,16 @@ import UPArrow from './images/up.png';
 import DOWNArrow from './images/down.png';
 import LEFTArrow from './images/left.png';
 import RIGHTArrow from './images/right.png';
-import bgImagew  from "./images/block.jpg";
+import blockImage from "./images/block.jpg";
+import crackrusImage from "./images/crackrus.jpg";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      blockPostition: [1, 11, 10, 14, 15, 16, 17, 20, 30, 40, 35, 39, 45, 55, 36, 46, 56, 65, 75, 84, 85, 94, 95],
+      cracktusostition: [23, 24, 25, 33, 34, 43, 44, 57, 58, 78, 79, 80, 82, 90, 92, 96]
+    };
   }
 
   componentDidMount() {
@@ -32,6 +37,7 @@ class App extends React.Component {
   }
 
   render() {
+    let counter = 0;
     return (
       <div className="App">
         <div className="container">
@@ -40,7 +46,17 @@ class App extends React.Component {
               {[...Array(20)].map((data, index) => {
                 return <div className="d-flex" key={index}>
                   {[...Array(30)].map((sdata, sindex) => {
-                    return <div id={"box" + sindex} key={sindex} style={{ borderRadius: 5, border: "1px solid #000", height: 25, width: 10, backgroundSize : "25px 25px" ,backgroundImage: "url(" + bgImagew + ")", backgroundPosition: "center center" , backgroundRepeat: "no-repeat" }} className="spacingbox flex-fill">&nbsp;</div>
+                     counter = counter + 1;
+                     let currentImage = "";
+                     if(this.state.blockPostition.indexOf(counter) > -1)
+                     {
+                        currentImage = blockImage;
+                     }
+                     if(this.state.cracktusostition.indexOf(counter) > -1)
+                     {
+                        currentImage = crackrusImage;
+                     }
+                    return <div id={"box" + sindex} key={sindex} style={{ borderRadius: 5, border: "1px solid #000", height: 25, width: 10, backgroundSize: "25px 25px", backgroundImage: "url(" + currentImage + ")", backgroundPosition: "center center", backgroundRepeat: "no-repeat" }} className="spacingbox flex-fill">&nbsp;</div>
                   })}
                 </div>
               })}
